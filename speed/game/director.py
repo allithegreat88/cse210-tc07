1d4ef338b43c3265051bb5b1a8bdc757a497340b
Mark <77358956+MarkAHoward@users.noreply.github.com>
2021-02-19 15:02:58 -0700

    director is still a mess

--- speed/game/director.py
+++ speed/game/director.py
@@ -1,1 +1,113 @@
+from game.input_service import InputService
+from game.output_service import OutputService
+from asciimatics.screen import Screen 
+from game.score import Score
+from game import constants
+from time import sleep
+from game.buffer import Buffer
+from game.word import word
 
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        Buffer (Buffer): The Word's target.
+        input_service (InputService): The input mechanism.
+        keep_playing (boolean): Whether or not the game can continue.
+        output_service (OutputService): The output mechanism.
+        score (Score): The current score.
+        Word (Word): The player or Word.
+    """
+
+    def __init__(self, input_service, output_service):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._buffer = Buffer()
+        self._input_service = input_service
+        self._keep_playing = True
+        self._output_service = output_service
+        self._score = Score()
+        self._word = Word()
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._keep_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+            sleep(constants.FRAME_LENGTH)
+
+    def _get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means getting the desired direction and moving the Word.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        letter = self._input_service.get_letter()
+        self._buffer.add_letter(letter)
+
+    def _do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means checking for a collision and updating the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._handle_body_collision()
+        self._handle_buffer_collision()
+        
+    def _do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means checking if there are stones left and declaring 
+        the winner.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._output_service.clear_screen()
+        self._output_service.draw_actor(self._buffer)
+        self._output_service.draw_actors(self._word.get_all())
+        self._output_service.draw_actor(self._score)
+        self._output_service.flush_buffer()
+
+    def _handle_body_collision(self):
+        """Handles collisions between the Word's head and body. Stops the game 
+        if there is one.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        head = self._word.get_head()
+        body = self._word.get_body()
+        for segment in body:
+            if head.get_position().equals(segment.get_position()):
+                self._keep_playing = False
+                break
+
+    def _handle_buffer_collision(self):
+        """Handles collisions between the Word's head and the Buffer. Grows the 
+        Word, updates the score and moves the Buffer if there is one.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        head = self._word.get_head()
+        if head.get_position().equals(self._buffer.get_position()):
+            points = self._buffer.get_points()
+            for n in range(points):
+                self._word.grow_tail()
+            self._score.add_points(points)
+            self._buffer.reset() 
